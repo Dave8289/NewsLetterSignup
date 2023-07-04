1fb751d6f666a008a2df584148566eeb2482cda6
Davinder Kumar <davindertwal@gmail.com>
2023-07-04 15:25:26 +0530

    final

--- index.js
+++ index.js
@@ -43,9 +43,6 @@ app.post("/", function (req, res) {
     } else {
       res.sendFile(__dirname + "/failure.html");
     }
-    response.on("data", function (data) {
-      console.log(JSON.parse(data));
-    });
   });
   request.write(jSonData);
   request.end();
